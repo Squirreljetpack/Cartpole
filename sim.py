c2601a6397a4101fd596dd5f9e5c75ce154c87ef
Richard <squirreljetpacks@gmail.com>
2022-12-08 06:56:27 -0500

    -mcode cleanup

--- sim.py
+++ sim.py
@@ -7,7 +7,11 @@ from math import degrees, radians, sin, cos
 import random
 from control import matlab, lqr
 
+
+#Simulates a cartpole. Player control, algorithmic control can be toggled via the up and down keys.
+
 pygame.init()
+# Miscellaneous settings
 HEIGHT = 400
 WIDTH = 600
 FPS = 100  # adjust tau in CartPole
@@ -19,7 +23,7 @@ USER = True
 SCALE = 20
 # scale is about 10 pixels per meter
 
-
+# Contains the logic of the cartpole: the simulation and control rules
 class CartPole:
     g = 9.81
     massCart = 1
@@ -29,23 +33,9 @@ class CartPole:
     forceMag = 30
     # variables in the linear approximation
     dissipation = 0
-    downwards = 1
+    downwards = -1
 
-    def ddt_calc(self, t, dt, f):
-        num = (
-            f * cos(t)
-            - self.massPole * self.poleLength * cos(t) * sin(t) * dt**2
-            + (self.massPole + self.massCart) * self.g * sin(t)
-        )
-        denum = self.poleLength * (self.massCart + self.massPole * sin(t) ** 2)
-        return num / denum
-
-    def ddx_calc(self, t, dt, f):
-        num = f + self.massPole * \
-            sin(t) * (self.g * cos(t) - self.poleLength * dt**2)
-        denum = self.massCart + self.massPole * sin(t) ** 2
-        return num / denum
-
+    # sim modes: default and linear
     def __init__(self, params=None, sim="default"):
         if params:
             self.state = params
@@ -65,7 +55,7 @@ class CartPole:
                 [
                     0,
                     -self.dissipation / self.massCart,
-                    -self.massPole * self.g / self.massCart,
+                    self.massPole * self.g / self.massCart,
                     0,
                 ],
                 [0, 0, 0, 1],
@@ -88,7 +78,7 @@ class CartPole:
                 0,
                 1 / self.massCart,
                 0,
-                self.downwards * -1 / (self.massCart * self.poleLength),
+                -self.downwards / (self.massCart * self.poleLength),
             ]
         )[:, np.newaxis]
 
@@ -97,8 +87,22 @@ class CartPole:
         )
         self.force_penalty = np.array([0.1])
 
-        # LQR matrices
         # reinforcement
+
+    def ddt_calc(self, t, dt, f):
+        num = (
+            f * cos(t)
+            - self.massPole * self.poleLength * cos(t) * sin(t) * dt**2
+            + (self.massPole + self.massCart) * self.g * sin(t)
+        )
+        denum = self.poleLength * (self.massCart + self.massPole * sin(t) ** 2)
+        return num / denum
+
+    def ddx_calc(self, t, dt, f):
+        num = f + self.massPole * \
+            sin(t) * (self.g * cos(t) - self.poleLength * dt**2)
+        denum = self.massCart + self.massPole * sin(t) ** 2
+        return num / denum
 
     def dstate(self, state, force):
         x, dx, t, dt = state
@@ -108,63 +112,48 @@ class CartPole:
         return np.array([dx, ddx, dt, ddt])
 
     def dstate_linear(self, state, force):
-        k = self.A_lin @ state + self.B_lin[:, 0] * force
-        print(k)
         return self.A_lin @ state + self.B_lin[:, 0] * force
 
     def setControl(self, mode, target=np.array([10, 0, 0, 0])):
         self.mode = mode
         if mode == "linear_place":
-            desired = np.array([[-3, -3.1, -3.2, -3.3]])
-            self.gain = np.array(matlab.place(self.A_lin, self.B_lin, desired))
+            #negative eigen values for stability
+            desired = np.array([[-2, -2.1, -2.2, -2.3]])
+            self.gain = np.squeeze(np.array(matlab.place(self.A_lin, self.B_lin, desired)))
+            self.target = target
+            print(self.A_lin, self.B_lin, self.gain)
+            # print("eigs",np.linalg.eig(self.A_lin-self.B_lin@self.gain[np.newaxis,:]))
         elif mode == "linear_qr":
             K, S, E = lqr(
                 self.A_lin, self.B_lin, self.state_penalty, self.force_penalty
             )
             self.gain = np.squeeze(np.array(K))
             self.target = target
-            print(self.A_lin, self.B_lin, self.gain, self.gain.shape)
+            print(self.A_lin, self.B_lin, self.gain)
+            # print("eigs:",S, E)
         elif mode == "reinforcement":
             pass
 
     def control(self):
         if "linear" in self.mode:
-            # print(self.A_lin)
-            print(
-                self.gain,
-                self.target,
-                self.state,
-                -(self.gain @ (self.state - self.target)),
-            )
-            return -(self.gain @ (self.state - self.target))
+            return (self.gain @ (self.target-self.state))
 
     def update(self, ext):
         if USER:
             force = ext * self.forceMag
         else:
             force = self.control()
-        # np array operations
         if self.sim == "linear":
             dstate = self.tau * self.dstate_linear(self.state, force)
         else:
             dstate = self.tau * self.dstate(self.state, force)
         self.state += dstate
-        print(system.state, force)
-
-
-Clock = pygame.time.Clock()
-
-displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Game")
-
-cart_img = pygame.image.load("transport-industrial-truck.png")
-pole_img = pygame.image.load("pole.png")
+        # print(system.state, force)
 
 
 class Pole(pygame.sprite.Sprite):
     def __init__(self, picture, length):
         super().__init__()
-        # state = posx, posy, angle, vel, acc, length
         self.height = 2 * length
         self.angle = 0
         self.center = (0, 0)
@@ -175,7 +164,6 @@ class Pole(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=self.center)
 
     def update(self, angle, dt=1 / FPS):
-        # self.state[2] = t[frame]
         self.angle = angle
         self.center = (
             P1.x,
@@ -189,7 +177,6 @@ class Pole(pygame.sprite.Sprite):
     def rotate(self):
         """Rotate the image of the sprite around its center."""
         self.image = pygame.transform.rotozoom(self.orig_image, self.angle, 1)
-        # self.image = pygame.transform.rotozoom(self.orig_image, self.angle + 180, 1)
         # Create a new rect with the center of the old rect.
         self.rect = self.image.get_rect(center=self.center)
 
@@ -198,16 +185,10 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, picture, width, height):
         super().__init__()
 
-        # state = posx, posy, width, height
         self.x, self.y = CART_POS[0], CART_POS[1]
         self.yv = 15
         self.image = pygame.transform.scale(picture, (width, height))
         self.rect = self.image.get_rect()
-
-    # def jump(self):
-    #     hits = pygame.sprite.spritecollide(self, platforms, False)
-    #     if hits:
-    #         self.yv = -15
 
     def update(self, x):
         self.x = x % WIDTH
@@ -230,11 +211,19 @@ class platform(pygame.sprite.Sprite):
     def move(self):
         pass
 
+#Game initialization
+Clock = pygame.time.Clock()
+displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game")
+cart_img = pygame.image.load("transport-industrial-truck.png")
+pole_img = pygame.image.load("pole.png")
+
 
 PT1 = platform()
-# config
-# system = CartPole([15, 0, np.pi, 2], sim="default")
+# System initialization, MODIFY IC HERE
+# system = CartPole([15, 0, np.pi, 2], sim="linear")
 system = CartPole([15, 0, 0, 2], sim="default")
+# system.setControl("linear_place")
 system.setControl("linear_qr")
 P1 = Player(cart_img, CART_WIDTH, CART_HEIGHT)
 P2 = Pole(pole_img, system.poleLength * SCALE)
@@ -246,10 +235,7 @@ cart_sprites.add(P2)
 platforms = pygame.sprite.Group()
 platforms.add(PT1)
 
-img_sprites = pygame.sprite.Group()
-img_sprites.add(P2)
-
-
+# Event handler for user input
 def get_input():
     pressed_keys = pygame.key.get_pressed()
     global USER
@@ -282,16 +268,12 @@ while True:
         displaysurface.blit(entity.surf, entity.rect)
 
     # cart system
-    # for entity in cart_sprites:
-    #     entity.update(system.state)
     P1.update(system.state[0] * SCALE)
     P2.update(degrees(system.state[2]))
     cart_sprites.draw(displaysurface)
 
     k = get_input()
     system.update(k)
-    # displaysurface.blit(P1.surf, P1.rect)
-    # img_sprites.draw(displaysurface)
 
     pygame.display.update()
     # Limits tick rate
